b7e841a78dd54709ea3f3b5c3a67f5acb5aa4936
FrozenBirdXD <118717731+FrozenBirdXD@users.noreply.github.com>
2024-08-18 22:14:20 +0200

    read binary file

--- src/main.c
+++ src/main.c
@@ -1,8 +1,25 @@
 #include <stdio.h>
 
+void read_file(const char* file_name);
+
 int
 main(int argc, char const* argv[]) {
-    printf("Hellodd");
-    printf("Hellodd");
+    read_file("../test-image.bmp");
     return 0;
+}
+
+void
+read_file(const char* file_name) {
+    FILE* file;
+
+    file = fopen(file_name, "rb");
+    if (file == NULL) {
+        printf("Error reading file '%s'", file_name);
+        return;
+    } else {
+        printf("File reading successful!");
+    }
+
+    fclose(file);
+    printf("\nFile closed");
 }
