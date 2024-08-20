65cda81437df1a65f7141d55de3e39056acc621e
FrozenBirdXD <118717731+FrozenBirdXD@users.noreply.github.com>
2024-08-20 14:09:20 +0200

    read more info from file

--- src/bitmaps.c
+++ src/bitmaps.c
@@ -23,9 +23,39 @@ read_file(const char* file_name) {
         fclose(file);
         return;
     }
-    printf("\nMagic num: 0x%d", fileheader.header_field);
+    printf("\nMagic num: 0x%x", fileheader.header_field);
+    printf("\nFilesize: %.2lfMB", fileheader.file_size / (double)1048576);
+    printf("\nDataoffset (byte): %d", fileheader.data_offset);
+    printf("\n");
 
-    printf("\nFilesize: %.2lfMB", fileheader.file_size / 1048576.0);
+    bmp_infoheader_t infoheader;
+    fread(&infoheader, sizeof(infoheader), 1, file);
+
+    if (infoheader.infoheader_size != 40) {
+        printf("\nError, infoheader has unsupported size: %dbytes", infoheader.infoheader_size);
+        fclose(file);
+        return;
+    }
+
+    printf("\nImage pixel resolution: %d x %d", infoheader.image_width, infoheader.image_height);
+    if (infoheader.num_color_planes != 1) {
+        printf("\nError, number of color planes not supported: %d", infoheader.num_color_planes);
+        fclose(file);
+        return;
+    }
+
+    printf("\nColor depth (bits per pixel): %d", infoheader.bits_per_pixel);
+    if (infoheader.bits_per_pixel != 24) {
+        printf("\nError, color depth of %d is not supported, only 24 is supported", infoheader.bits_per_pixel);
+        fclose(file);
+        return;
+    }
+
+    if (infoheader.compression_method != 0) {
+        printf("\nError, compression method unsupported, header value: %d", infoheader.compression_method);
+        fclose(file);
+        return;
+    }
 
     fclose(file);
     printf("\nFile closed");
--- src/bitmaps.h
+++ src/bitmaps.h
@@ -15,6 +15,29 @@ typedef struct {
     uint32_t data_offset;
 } bmp_fileheader_t;
 
+typedef struct {
+    uint32_t infoheader_size;
+    int32_t image_width;
+    int32_t image_height;
+    /* Must be 1 */
+    uint16_t num_color_planes;
+    uint16_t bits_per_pixel;
+    uint32_t compression_method;
+    uint32_t bitmap_data_size;
+    /* In pixels per meter */
+    int32_t horizontal_resolution;
+    /* In pixels per meter */
+    int32_t vertical_resolution;
+    uint32_t num_of_colors;
+    uint32_t num_of_important_colors;
+} bmp_infoheader_t;
+
+typedef struct {
+    uint8_t r;
+    uint8_t g;
+    uint8_t b;
+} rgb_pixel_t;
+
 #pragma pack(pop)
 
 void read_file(const char* file_name);
