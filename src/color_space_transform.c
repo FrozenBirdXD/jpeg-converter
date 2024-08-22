c3bfbb2aa956543b4e8da390e6ef4defc87bf8b4
FrozenBirdXD <118717731+FrozenBirdXD@users.noreply.github.com>
2024-08-22 16:23:39 +0200

    create boilerplate for color space conversion

---
+++ src/color_space_transform.c
@@ -0,0 +1,7 @@
+#include <stdlib.h>
+
+#include "bitmaps.h"
+#include "color_space_transform.h"
+
+ycbcr_image_t*
+color_space_transform(const rgbimage_t* rgb_image) {}
--- src/main.c
+++ src/main.c
@@ -2,14 +2,13 @@
 #include <stdlib.h>
 
 #include "bitmaps.h"
+#include "color_space_transform.h"
 
 int
 main(int argc, char const* argv[]) {
-    image_t* image = read_file("../test-image.bmp");
+    rgbimage_t* rgb_image = read_bitmap("../test-image.bmp");
+    ycbcr_image_t* ycbcr_image = color_space_transform(rgb_image);
 
-    for (int i = 0; i < image->width; ++i) {
-        free(image->data[i]);
-    }
-    free(image->data);
+    free_rgb_image(rgb_image);
     return 0;
 }
