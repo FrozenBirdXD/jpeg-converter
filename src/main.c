57d3ad3e4883916ded5be42e9349c67834bffa9f
FrozenBirdXD <118717731+FrozenBirdXD@users.noreply.github.com>
2024-08-22 14:49:03 +0200

    free image data after use

--- src/main.c
+++ src/main.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "bitmaps.h"
 
 int
 main(int argc, char const* argv[]) {
-    read_file("../test-image.bmp");
+    image_t* image = read_file("../test-image.bmp");
+
+    for (int i = 0; i < image->width; ++i) {
+        free(image->data[i]);
+    }
+    free(image->data);
     return 0;
 }
